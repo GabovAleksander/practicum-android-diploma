6c128919308a7a06533ea295141258f2010ea0ff
RinatGordey <140505799+RinatGordey@users.noreply.github.com>
2024-09-18 08:31:02 +0500

    Исправление detect2

--- app/src/main/java/ru/practicum/android/diploma/filter/ui/chooseaplaceofwork/ChoosingAPlaceOfWorkFragment.kt
+++ app/src/main/java/ru/practicum/android/diploma/filter/ui/chooseaplaceofwork/ChoosingAPlaceOfWorkFragment.kt
@@ -40,13 +40,17 @@ class ChoosingAPlaceOfWorkFragment : CustomFragment<FragmentChoosingAPlaceOfWork
 
     private fun createTextWatcher(block: (String) -> Unit): TextWatcher {
         return object : TextWatcher {
-            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+                // для detect
+            }
 
             override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
                 block(s?.toString() ?: "")
             }
 
-            override fun afterTextChanged(s: Editable?) {}
+            override fun afterTextChanged(s: Editable?) {
+                // для detect
+            }
         }
     }
 
